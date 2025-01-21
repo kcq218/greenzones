46542ff1678a2e1760eec2f6dc6eab6352510c40
kcq218 <khangquach218@gmail.com>
2025-01-21 01:27:25 -0800

    Finished V1

--- GreenZones/GreenZones/Models/ViewModels/EditShotTypeViewModel.cs
+++ GreenZones/GreenZones/Models/ViewModels/EditShotTypeViewModel.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GreenZones.Models.ViewModels
 {
     public class EditShotTypeViewModel
     {
-        public int UserId { get; set; }
         public int ShotTypeId { get; set; }
+        public int UserId { get; set; }
         public string? DisplayName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a shot type.")]
+        [StringLength(50, ErrorMessage = "Name length can't be more than 50.")]
         public string? ShotType { get; set; }
     }
 }
